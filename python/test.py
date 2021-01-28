1b97d77615a8c9489c8e54ab5b1a93d5a34bfb63
kibrael <kibrael.roell@cfpb.gov>
2021-01-27 19:31:28 -0500

    test

--- python/test.py
+++ python/test.py
@@ -10,26 +10,27 @@ import requests as re
 from census_functions import CensusTools
 
 Censustool = CensusTools()
-
+#Censustool.config_data["ALL_YEARS"] = 2018
 #download census files
 Censustool.get_ffiec_census_file(years=Censustool.config_data["ALL_YEARS"], download=True, unzip=False, move=False)
 
 #Unzip Census files
-Censustool.get_ffiec_census_file(years=Censustool.config_data["ALL_YEARS"], download=False, unzip=True, move=False)
+#Censustool.get_ffiec_census_file(years=Censustool.config_data["ALL_YEARS"], download=False, unzip=True, move=False)
 #2017 is zipped twice, the second call unzips the sub archive
-Censustool.get_ffiec_census_file(years=["2017"], download=False, unzip=True, move=False)
+#Censustool.get_ffiec_census_file(years=["2017"], download=False, unzip=True, move=False)
 
 #move Census files and empty folders
-Censustool.get_ffiec_census_file(years=Censustool.config_data["ALL_YEARS"], download=False, unzip=False, move=True)
+#Censustool.get_ffiec_census_file(years=Censustool.config_data["ALL_YEARS"], download=False, unzip=False, move=True)
 
 #extract desired columns from census files
-extracts_dict = Censustool.extract_census_fields(years=Censustool.config_data["ALL_YEARS"]) #sep can be changed to comma for CSV output
+#extracts_dict = Censustool.extract_census_fields(years=Censustool.config_data["ALL_YEARS"]) #sep can be changed to comma for CSV output
 
 #download OMB MSA MD delineation files
 Censustool.get_census_omb_delineation_file(years=Censustool.config_data["ALL_YEARS"])
 
-#combine OMB and FFIEC census data
-Censustool.combine_omb_ffiec(years=Censustool.config_data["ALL_YEARS"])
+#combine OMB and FFIEC census data and create MSA/MD name description files
+census_df_dict = Censustool.combine_omb_ffiec(years=Censustool.config_data["ALL_YEARS"], sep=",")
+census_df_dict = Censustool.combine_omb_ffiec(years=Censustool.config_data["ALL_YEARS"], sep="|")
 
 #load combined data to database
-Censustool.load_to_db(years=Censustool.config_data["ALL_YEARS"])
+#Censustool.load_to_db(years=Censustool.config_data["ALL_YEARS"])
